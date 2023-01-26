c9380acd9d791349a66e4eb3359d2328efe3d97e
Lucas Maieski <lucmaieski@gmail.com>
2023-01-26 18:08:08 -0300

    Create header component test

---
+++ __tests__/testComponents/header/header.test.tsx
@@ -0,0 +1,4 @@
+describe("Test header component", () => {
+  it("must have a title called `Almaden...`", () => {});
+  it("must have a search input with placeholder named `Pesquisar no meu cofre`", () => {});
+});
