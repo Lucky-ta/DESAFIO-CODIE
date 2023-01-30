1d6cbb0c000b513ca4a941fd3362790222666ea7
Lucas Maieski <oficiallucasmaieski@gmail.com>
2023-01-30 19:11:24 -0300

    Clear filSystemFucntions file

--- src/utils/fileSystemFunctions.ts
+++ src/utils/fileSystemFunctions.ts
@@ -1,37 +1,5 @@
-import axios from "axios";
 import { DataShape, FileShape } from "../interfaces/interfaces";
-import API_URLS from "../services/api/apiUrls/urls";
 import { addPassword, createFile, getFiles } from "../services/api/filesApi";
-
-// export const addPasswordToFile = async (newCurrentFile: DataShape, fileName: string) => {
-    // const fileExists = Object.keys(files).includes(newCurrentFile.file);
-  
-    // if (fileExists) {
-    //   Adiciona senha ao arquivo existente
-    //   files[newCurrentFile.file] = [...files[newCurrentFile.file], newCurrentFile];
-    //   return;
-    // }
-    // Cria novo arquivo
-    // files[newCurrentFile.file] = [newCurrentFile];
-
-//     const filesApi = await getFiles();
-//     const fileExists = Object.keys(filesApi).includes(newCurrentFile.file);
-    
-//     if (fileExists) {
-//       const addPass = await addPasswordToFile(newCurrentFile, fileName);
-//       console.log("add password to exist file: ", addPass);
-
-//     }
-
-//     const newFile = {
-//       [fileName]: [newCurrentFile]
-//     };
-
-//     const createFileResult = await createFile(newFile)
-//     console.log("create file with password: ", createFileResult);
-
-    
-// };
 
 export const addPasswordToFile = async (fileName: string, password: DataShape) => {
   const allFiles = await getFiles();
@@ -48,13 +16,6 @@ export const addPasswordToFile = async (fileName: string, password: DataShape) =
   return console.log("criado: ", result);
 
 };
-
-
-
-
-
-
-
 
 export const deletePassword = (files: FileShape, key: string, passwordIndex: number, setFiles: any) => {
   const { [key]: deletedPasswords, ...newFiles } = files;
